8b6976271fa4b45b4fb02cdecbf611001a88244d
tmont <tommy.mont@gmail.com>
2014-01-04 21:51:55 -0800

    constructor instead of static object

--- audio-manager.js
+++ audio-manager.js
@@ -1,69 +1,138 @@
 (function(window) {
 
-	var AudioContext = window.AudioContext || window.webkitAudioContext;
-	window.AudioManager = {
-		createContext: function() {
-			return new AudioContext();
-		},
+	function AudioManager(context) {
+		this.context = context || AudioManager.createContext();
+		this.files = {};
+		this.sources = {};
+	}
 
+	AudioManager.prototype = {
 		load: function(url, options, callback) {
 			if (typeof(options) === 'function') {
 				callback = options;
 				options = {};
 			}
+			options = options || {};
 
-			var req = new XMLHttpRequest();
+			var req = new XMLHttpRequest(),
+				self = this;
 			req.open('GET', url, true);
 			req.responseType = 'arraybuffer';
 
-			var context = options.context || this.createContext();
-			options.context = context;
 			req.onload = function() {
-				context.decodeAudioData(req.response, function(buffer) {
-					callback(null, buffer, options);
+				self.context.decodeAudioData(req.response, function(buffer) {
+					var file = {
+						name: options.name || url,
+						url: url,
+						buffer: buffer
+					};
+
+					self.files[file.name] = file;
+
+					callback && callback(null, file);
 				}, callback);
 			};
 
 			req.send(null);
 		},
 
-		play: function(options, callback) {
+		destroySource: function(source, name) {
+			source.disconnect();
+			var index = this.sources[name].indexOf(source);
+			if (index >= 0) {
+				this.sources[name].splice(index, 1);
+			}
+		},
+
+		play: function(name, options, callback) {
+			var self = this;
+
 			if (typeof(options) === 'function') {
 				callback = options;
 				options = {};
 			}
+			options = options || {};
 
-			if (options.buffer) {
-				options.context = options.context || this.createContext();
-				play(options.buffer);
-			} else if (options.url) {
-				this.load(options.url, options, function(err, buffer) {
+			var file = this.files[name];
+			if (!file) {
+				this.load(name, function(err, file) {
 					if (err) {
 						callback && callback(err);
 						return;
 					}
 
-					play(buffer);
+					play(file);
 				});
 			} else {
-				throw new Error('One of options.url or options.buffer must be set');
+				play(file);
 			}
 
-			function play(buffer) {
-				var source = options.context.createBufferSource();
-				source.buffer = buffer;
-				source.connect(options.context.destination);
+			function play(file) {
+				var source;
+				if (options.override && this.sources[name] && this.sources[name].length) {
+					source = this.sources[name][0];
+				} else {
+					source = self.context.createBufferSource();
+					source.buffer = file.buffer;
+					source.connect(self.context.destination);
+				}
+
 				if (options.loop) {
 					source.loop = true;
 				}
+
 				source.start(0);
-				callback(null, { buffer: buffer, source: source }, options);
+
+				if (!options.override) {
+					if (!self.sources[file.name]) {
+						self.sources[file.name] = [];
+					}
+
+					self.sources[file.name].push(source);
+				}
+
+				if (!options.persistent) {
+					source.onended = function() {
+						self.destroySource(source, name);
+					};
+				}
+
+				callback && callback(null, source);
 			}
 		},
 
-		stop: function(source) {
-			source.stop(0);
+		stop: function(sourceOrName, options) {
+			options = options || {};
+
+			if (sourceOrName.stop) {
+				sourceOrName.stop(0);
+			} else {
+				var sources = this.sources[sourceOrName];
+				if (!sources || !sources.length) {
+					throw new Error('No sources for ' + sourceOrName);
+				}
+
+				sources[0].stop(0);
+				if (options.all) {
+					for (var i = 1; i < sources.length; i++) {
+						sources[i].stop(0);
+					}
+				}
+			}
 		}
 	};
+
+	AudioManager.createContext = function() {
+		if (window.AudioContext) {
+			return new window.AudioContext();
+		}
+		if (window.webkitAudioContext) {
+			return new window.webkitAudioContext();
+		}
+
+		throw new Error('Your browser doesn\'t support AudioContext');
+	};
+
+	window.AudioManager = AudioManager;
 
 }(window));
